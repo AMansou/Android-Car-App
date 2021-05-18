4d9d204a0eac177345a2892c0c2e9df615691316
Saeed <AbdulraheemSaeed3@gmail.com>
2021-05-18 12:33:18 +0300

    some admin page additions and fixing missing files

---
+++ app/src/main/java/com/example/brojekt/AdminPage_1172631_1171821.java
@@ -0,0 +1,18 @@
+package com.example.brojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPage_1172631_1171821 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page_1172631_1171821);
+
+
+    }
+}
