4f106043bc8061c2f43ef4003d7fb0c9761bcf1d
Saeed <AbdulraheemSaeed3@gmail.com>
2021-05-21 18:56:09 +0300

    FIXED CUSTOMER DATABASE AND ADDED A FUNCTION TO READ CUSTOMER FROM DATABASE GIVEN AN ID

--- app/src/main/java/com/example/brojekt/DataBaseHelper.java
+++ app/src/main/java/com/example/brojekt/DataBaseHelper.java
@@ -16,7 +16,9 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE CUSTOMER(ID LONG PRIMARY KEY,NAME TEXT, PHONE TEXT,GENDER TEXT)") ;
+        sqLiteDatabase.execSQL("CREATE TABLE CUSTOMER(ID LONG PRIMARY KEY,FIRSTNAME TEXT," +
+                "LASTNAME TEXT, PHONE TEXT, PASSWORD TEXT, GENDER TEXT, COUNTRY TEXT, CITY TEXT," +
+                "ISADMIN BOOLEAN)") ;
         sqLiteDatabase.execSQL("CREATE TABLE CARS(MODEL TEXT PRIMARY KEY, MAKE TEXT, YEAR TEXT" +
                 ", PRICE INT, DISTANCE INT, ACCIDENTS BOOLEAN, OFFERS BOOLEAN)");
     }
@@ -24,13 +26,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase sqLiteDatabase = getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("EMAIL", customer.getEmail());
-        contentValues.put("FIRST NAME", customer.getFirstName());
-        contentValues.put("LAST NAME", customer.getLastName());
+        contentValues.put("FIRSTNAME", customer.getFirstName());
+        contentValues.put("LASTNAME", customer.getLastName());
         contentValues.put("PHONE", customer.getPhone());
         contentValues.put("PASSWORD", customer.getPassword());
         contentValues.put("GENDER", customer.getGender());
         contentValues.put("COUNTRY", customer.getCountry());
         contentValues.put("CITY", customer.getCity());
+        contentValues.put("ISADMIN", customer.isAdmin());
         sqLiteDatabase.insert("CUSTOMER", null, contentValues);
     }
 
@@ -39,6 +42,19 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         //ontentValues contentValues = new ContentValues();
         //contentValues.put("ID", customer.getID());
         sqLiteDatabase.delete("CUSTOMER", "ID=?",new String[] {id});
+    }
+
+    public Customer getCustomer(int id) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.query("CUSTOMER", new String[] { "ID", "EMAIL",
+                        "FIRSTNAME", "LASTNAME", "PHONE", "PASSWORD", "GENDER", "COUNTRY", "CITY", "ISADMIN" }, "ID" + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Customer co1 = new Customer(cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(5),
+                cursor.getString(7), cursor.getString(8), cursor.getString(4), cursor.getString(6));
+        return co1;
     }
     public Cursor getAllCustomers() {
         SQLiteDatabase sqLiteDatabase = getReadableDatabase();
