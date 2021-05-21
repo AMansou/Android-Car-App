ce793c325679f925ddba235a543ce0d1e849886f
Ahmad <hondace33@hotmail.com>
2021-05-21 19:27:09 +0300

    lol

---
+++ app/src/main/java/com/example/brojekt/CarFrag.java
@@ -0,0 +1,43 @@
+package com.example.brojekt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CarFrag extends Fragment {
+    private String s;
+    public CarFrag() {
+
+    }
+
+    public void setText(String s){
+        this.s=s;
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = (View)inflater.inflate(R.layout.fragment_car, container, false);
+
+        //set the text of your text view
+        TextView textView = (TextView) view.findViewById(R.id.hola);
+        textView.setText(s);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+
+
+}
