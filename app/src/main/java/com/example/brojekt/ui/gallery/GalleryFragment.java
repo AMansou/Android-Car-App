b250b8aeba412047ae60743b52db1bf78e1bdfbe
Ahmad <hondace33@hotmail.com>
2021-05-22 16:36:18 +0300

    Fixed again

--- app/src/main/java/com/example/brojekt/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/brojekt/ui/gallery/GalleryFragment.java
@@ -40,21 +40,19 @@ public class GalleryFragment extends Fragment {
         View root=inflater.inflate(R.layout.fragment_gallery,container,false);
         LinearLayout lol=(LinearLayout) root.findViewById(R.id.lol);
         final Button addButton =new Button(container.getContext());
-        DataBaseHelper dataBaseHelper =new DataBaseHelper(container.getContext(),"CUSTOMER",null,1);
-        Cursor c=dataBaseHelper.getAllCustomers();
+        DataBaseHelper dataBaseHelper =new DataBaseHelper(container.getContext(),"CARS",null,1);
+        Cursor c=dataBaseHelper.getAllCars();
 
 
         Button  button;
         ArrayList<Button> buttons = new ArrayList<Button>();
         linearLayout=new LinearLayout(container.getContext());
-        System.out.println("LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL");
         int i = 0;
         while(c.moveToNext()){
 
             button=new Button(container.getContext());
             buttons.add(button);
             buttons.get(i).setText(c.getString(1));//String.valueOf(i));
-            System.out.println(c.getString(0));
             if(i%4==0){
                 lol.addView(linearLayout);
                 linearLayout=new LinearLayout(container.getContext());
@@ -71,7 +69,7 @@ public class GalleryFragment extends Fragment {
         final FragmentManager fragmentManager = myContext.getSupportFragmentManager();
         String str;
         final CarFrag firstFragment = new CarFrag();
-        c=dataBaseHelper.getAllCustomers();
+        c=dataBaseHelper.getAllCars();
         i=0;
         while ( c.moveToNext()) {
             str="lolll";//"Model: "+c.getString(1)+"\nMake: "+c.getString(0)+"\nYear: "+c.getString(2)+"\nPrice: "+c.getString(3)+"$\nDistance in Km: "+c.getString(4)+"\n"+ c.getString(5)+"\n"+c.getString(6)+"\n";
