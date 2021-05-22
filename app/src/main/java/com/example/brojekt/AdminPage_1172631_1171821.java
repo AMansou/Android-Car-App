d2114d18521ce7084ce733356bd401cb4d4a1b64
Saeed <AbdulraheemSaeed3@gmail.com>
2021-05-22 18:28:14 +0300

    nothing important

--- app/src/main/java/com/example/brojekt/AdminPage_1172631_1171821.java
+++ app/src/main/java/com/example/brojekt/AdminPage_1172631_1171821.java
@@ -2,49 +2,64 @@ package com.example.brojekt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
-public class AdminPage_1172631_1171821 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class AdminPage_1172631_1171821 extends AppCompatActivity {
 
-
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_page_1172631_1171821);
-        navigationView = findViewById(R.id.navigation_view_admin);
-        drawerLayout = findViewById(R.id.admin_drawer);
-
+        setContentView(R.layout.activity_customer_home_1172631_1171821);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.delete_customer:{
-                System.out.println("delete customer");
-                break;
-            }
-            case R.id.add_admin:{
-                System.out.println("add admin");
-                break;
-            }
-            case R.id.view_reserves:{
-                System.out.println("reserves");
-                break;
-            }
-            case R.id.logout_admin:{
-                System.out.println("log admin out");
-                break;
-            }
-        }
-        return false;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.customer_home_1172631_1171821, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
