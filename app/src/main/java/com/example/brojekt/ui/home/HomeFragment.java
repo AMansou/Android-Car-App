bc93b4f1bc5258a67e6cbd54235c341ad7872c73
Saeed <AbdulraheemSaeed3@gmail.com>
2021-05-23 13:15:56 +0300

    delete customer fragment in admin is implemented

--- app/src/main/java/com/example/brojekt/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/brojekt/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.brojekt.ui.home;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +17,48 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.brojekt.AdminHome_1172631_1171821;
+import com.example.brojekt.DataBaseHelper;
+import com.example.brojekt.Login_1172631_1171821;
 import com.example.brojekt.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private EditText emailcustomer;
+    private Button removecustomer;
+    Context thiscontext;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+                             final ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        emailcustomer = root.findViewById(R.id.editTextRemoveCustomer);
+        removecustomer = root.findViewById(R.id.button_remove_customer);
+        thiscontext = container.getContext();
+
+        removecustomer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DataBaseHelper db = new DataBaseHelper(thiscontext,"CUSTOMER",null,1);
+                String email = emailcustomer.getText().toString();
+                Cursor allCustomersCursor = db.getAllCustomers();
+                while (allCustomersCursor.moveToNext()){
+
+                    if(email.trim().equalsIgnoreCase(allCustomersCursor.getString(0))){
+                        break;
+                    }
+                    if(allCustomersCursor.isLast())
+                    {
+                        Toast.makeText(thiscontext, " This customer does not exist",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                db.deleteCustomer(email);
+                Toast.makeText(thiscontext, email + "has been removed", Toast.LENGTH_LONG).show();
             }
         });
+
         return root;
     }
 }
