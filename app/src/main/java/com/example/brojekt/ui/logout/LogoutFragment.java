2249b8d4118c62bda72c4f69a39cc17b7ba8d3e8
Saeed <AbdulraheemSaeed3@gmail.com>
2021-05-23 17:42:37 +0300

    LogoutFragment implemented

--- app/src/main/java/com/example/brojekt/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/brojekt/ui/logout/LogoutFragment.java
@@ -1,24 +1,40 @@
 package com.example.brojekt.ui.logout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.brojekt.AdminHome_1172631_1171821;
+import com.example.brojekt.DataBaseHelper;
+import com.example.brojekt.Login_1172631_1171821;
 import com.example.brojekt.R;
 public class LogoutFragment extends Fragment {
-    TextView text_logout;
+    private Button logout;
+    Context thiscontext;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_logout, container, false);
-        text_logout = root.findViewById(R.id.text_logout);
-        text_logout.setText("Are you sure you want to logout?");
+        thiscontext = container.getContext();
+        logout = root.findViewById(R.id.button_logout_admin);
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent( thiscontext, Login_1172631_1171821.class);
+                thiscontext.startActivity(intent);
+            }
+        });
         return root;
     }
 }
